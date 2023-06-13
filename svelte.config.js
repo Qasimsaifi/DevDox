9c17b9cf5ad4953088449aa9cdcaad218fb097ee
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-14 01:27:02 +0530

    added

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+import path from 'path';
+import dotenv from 'dotenv';
 
-/** @type {import('@sveltejs/kit').Config} */
+dotenv.config({ path: path.resolve('.env') });
+
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      // see the 'Deployment configuration' section below
+    })
+  }
 };
 
 export default config;
