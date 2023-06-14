1a899642608d5454f68425a01823a6e997769a37
Qasimsaifi <106295428+Qasimsaifi@users.noreply.github.com>
2023-06-15 00:47:29 +0530

    sended

--- src/routes/snippet/[snippetSlug]/+page.svelte
+++ src/routes/snippet/[snippetSlug]/+page.svelte
@@ -1,42 +1,31 @@
 <script>
-  import Navbar from '../../../components/Navbar.svelte';
   import { onMount, afterUpdate } from 'svelte';
-  import { page } from '$app/stores';
   import { goto } from '$app/navigation';
   import { getCookie } from '../../../utils/cookies';
+  import { page } from '$app/stores';
+  import Navbar from '../../../components/Navbar.svelte';
 
   let loading = true;
   let data = null;
+  let comments = [];
   const dataSlug = $page.params.snippetSlug;
-  onMount(async () => {
-    accessToken = getCookie('access_token');
 
-    if (!accessToken) {
-      goto('/login');
-    } else {
-      fetchUser();
-    }
+  onMount(async () => {
+    fetchData();
   });
-  // Fetch data with the specified slug
+
   const fetchData = async () => {
     try {
-      const response = await fetch(`https://devdox.up.railway.app/api/v1/snippets/snippet/?slug=${dataSlug}`, {
-        headers: {
-          Authorization: `Bearer ${getCookie('access_token')}`
-        }
-      });
+      const response = await fetch(`https://devdox.up.railway.app/api/v1/snippets/snippet/?slug=${dataSlug}`);
       const responseData = await response.json();
-      data = responseData.results[0]; // Get the first item from the results array
+      data = responseData.results[0];
+      fetchComments(); // Fetch comments for the specific snippet
     } catch (error) {
       console.error(error);
     } finally {
       loading = false;
     }
   };
-
-  onMount(() => {
-    fetchData();
-  });
 
   afterUpdate(() => {
     if (data) {
@@ -85,18 +74,60 @@
 
   function initializePrism() {
     const script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js';
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.24.1/prism.min.js';
     document.head.appendChild(script);
+  }
+
+  // Function to fetch comments for the specific snippet
+  async function fetchComments() {
+    try {
+      const response = await fetch(`https://devdox.up.railway.app/api/v1/snippets/comments/?snippet=${data.id}`);
+      const responseData = await response.json();
+      comments = responseData;
+      
+      // Scroll to the bottom of the comments section
+      const commentsContainer = document.querySelector('.comments-section');
+      commentsContainer.scrollTop = commentsContainer.scrollHeight;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Function to handle comment submission
+  async function submitComment(event) {
+    event.preventDefault();
+    const commentContent = event.target.elements.comment.value;
+
+    try {
+      const response = await fetch('https://devdox.up.railway.app/api/v1/snippets/comments/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          snippet: data.id,
+          content: commentContent
+        })
+      });
+
+      if (response.ok) {
+        event.target.elements.comment.value = ''; // Clear the comment input field
+        fetchComments(); // Fetch comments again to update the UI
+      } else {
+        console.error('Comment submission failed');
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
 </script>
 
-<Navbar/>
+<Navbar />
 
 {#if loading}
   <p>Loading...</p>
 {:else if data}
   <div class="card">
-      <h4>{data.title}</h4>
     <div class="card-content">
       <h4>{data.title}</h4>
       <div>{@html data.content}</div>
@@ -108,78 +139,143 @@
       </div>
     </div>
   </div>
+  <div class="comments-section">
+    <h4>Comments</h4>
+    {#each comments as comment (comment.id)}
+      <div class="comment">
+        <div class="comment-content">
+          <p class="comment-text">{comment.content}</p>
+        </div>
+      </div>
+    {/each}
+    <form on:submit={submitComment}>
+      <div class="comment-input">
+        <input type="text" name="comment" placeholder="Enter your comment" />
+        <button type="submit">
+          <i class="fas fa-paper-plane"></i>
+        </button>
+      </div>
+    </form>
+  </div>
 {:else}
   <p>Not found</p>
 {/if}
 
-
-
-
 <style>
-  /* Loading message */
-  p {
-    font-size: 20px;
-    text-align: center;
-    margin-top: 50px;
-  }
+/* Loading message */
+p {
+  font-size: 20px;
+}
 
-  /* Card */
-  .card {
-    padding: 20px;
-    margin-bottom: 20px;
-    width: 80vw;
-    margin-left: auto;
-    margin-right: auto;
-  }
+/* Card */
+.card {
+  padding: 20px;
+  margin-bottom: 20px;
+  width: 80vw;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #1e1e1e;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  color: #fff;
+}
 
-  /* Card title */
-  .card h4 {
-    font-size: 24px;
-    margin-bottom: 10px;
-  }
+/* Card title */
+.card h4 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
 
-  /* Card content */
-  .card-content {
-    margin-top: 20px;
-  }
+/* Card content */
+.card-content {
+  margin-top: 20px;
+}
 
-  /* Copy button */
-  .copy-button {
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    padding: 10px;
-    margin-top: 10px;
-    cursor: pointer;
-    transition: background-color 0.3s;
-    margin-left: auto;
-    margin-right: 0;
-  }
+/* Copy button */
+.copy-button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+  margin-top: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  margin-left: auto;
+  margin-right: 0;
+}
 
-  .copy-button i {
-    margin-right: 5px;
-  }
+.copy-button i {
+  margin-right: 5px;
+}
 
-  /* Code snippet */
-  .code-snippet {
-    background-color: #272b33;
-    border-radius: 10px;
-    padding: 10px;
-    margin-top: 10px;
-  }
+/* Code snippet */
+.code-snippet {
+  background-color: #272b33;
+  border-radius: 10px;
+  padding: 10px;
+  margin-top: 10px;
+}
 
-  /* Responsive styles */
-  @media (max-width: 768px) {
-    .card {
-      width: 100vw;
-    }
+/* Comments section */
+.comments-section {
+  margin-top: 20px;
+  margin-bottom: 50px;
+  width: 80vw;
+  margin-right: auto;
+  margin-left: auto;
+  overflow-y: auto; /* Enable vertical scrolling */
+}
 
-    .copy-button {
-      margin-left: auto;
-    }
-  }
+.comments-section h4 {
+  font-size: 20px;
+  margin-bottom: 10px;
+  color: #fff;
+}
 
-  /* Other styles */
-  /* ... */
+.comments-section .comment {
+  display: flex;
+  margin-top: 5px;
+  height: 25px;
+}
+
+.comments-section .comment-content {
+  display: flex;
+  flex-direction: column;
+}
+
+.comments-section .comment-text {
+  font-size: 18px;
+  color: #0c0c0c;
+}
+
+/* Comment input */
+.comment-input {
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  border: 2px solid #007bff;
+  border-radius: 7px;
+}
+
+.comment-input input {
+  flex: 1;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  margin-right: 10px;
+}
+
+.comment-input button {
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.comment-input button i {
+  margin-right: 5px;
+}
 </style>
